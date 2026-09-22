4a44b4f4209d65bd173b6062159af6a55b2b2a9b
jamesiit <james.20232826@iit.ac.lk>
2026-09-22 20:16:06 +0530

    Lecture 1: Spring Boot project created

---
+++ weekone/src/main/java/uk/ac/westminster/productsapi/WeekoneApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.westminster.productsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekoneApplication.class, args);
+    }
+
+}
---
+++ weekone/src/test/java/uk/ac/westminster/productsapi/WeekoneApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.westminster.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
